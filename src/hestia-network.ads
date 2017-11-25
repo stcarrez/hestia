bed517385bd0a1ed38de12b28260497e4bb285d4
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-11-25 16:18:08 +0100

    Declare NTP_Client_Type to make a DNS request and manage the NTP client Make the DHCP variable a private instance

--- src/hestia-network.ads
+++ src/hestia-network.ads
@@ -20,6 +20,8 @@ with Interfaces;
 with Net.Buffers;
 with Net.Interfaces.STM32;
 with Net.DHCP;
+with Net.NTP;
+with Net.DNS;
 package Hestia.Network is
 
    use type Interfaces.Unsigned_32;
@@ -30,15 +32,37 @@ package Hestia.Network is
    --  The Ethernet interface driver.
    Ifnet     : aliased Net.Interfaces.STM32.STM32_Ifnet;
 
-   --  The DHCP client used by Hestia.
-   Dhcp      : aliased Net.DHCP.Client;
-
    --  Initialize and start the network stack.
    procedure Initialize;
+
+private
 
    --  The task that waits for packets.
    task Controller with
      Storage_Size => (16 * 1024),
      Priority => System.Default_Priority;
+
+   type NTP_Client_Type is limited new Net.DNS.Query with record
+      Server : aliased Net.NTP.Client;
+      Port   : Net.Uint16 := Net.NTP.NTP_PORT;
+   end record;
+
+   --  Save the answer received from the DNS server.  This operation is called for each answer
+   --  found in the DNS response packet.  The Index is incremented at each answer.  For example
+   --  a DNS server can return a CNAME_RR answer followed by an A_RR: the operation is called
+   --  two times.
+   --
+   --  This operation can be overriden to implement specific actions when an answer is received.
+   overriding
+   procedure Answer (Request  : in out NTP_Client_Type;
+                     Status   : in Net.DNS.Status_Type;
+                     Response : in Net.DNS.Response_Type;
+                     Index    : in Natural);
+
+   --  The DHCP client used by Hestia.
+   Dhcp      : aliased Net.DHCP.Client;
+
+   --  NTP client based on the NTP server provided by DHCP option (or static).
+   Time_Ntp  : aliased NTP_Client_Type;
 
 end Hestia.Network;
