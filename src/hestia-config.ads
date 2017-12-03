1531f294a12ee22b23db5f3c2484fe95f49a29cb
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-12-03 09:11:41 +0100

    New package for the configuration

---
+++ src/hestia-config.ads
@@ -0,0 +1,27 @@
+-----------------------------------------------------------------------
+--  hestia-config -- Hestia configuration
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package Hestia.Config is
+
+   --  Maximum number of zones to control.
+   MAX_ZONES : constant := 3;
+
+   --  Simple timezone correction (+1 hour, not DST support yet).
+   TIME_ZONE_CORRECTION : constant := 1;
+
+end Hestia.Config;
