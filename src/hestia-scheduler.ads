e95c5248b5c36e12eb6857633c68941699a4e5f4
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-11-18 09:14:25 +0100

    Declare Day_Time and several operations Declare Get_State and Set_State to get and set the schedule

--- src/hestia-scheduler.ads
+++ src/hestia-scheduler.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  hestia-scheduler -- Hestia Scheduler
---  Copyright (C) 2016, 2017 Stephane Carrez
+--  Copyright (C) 2016, 2017, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,11 +25,40 @@ package Hestia.Scheduler is
    --  The schedule state.
    type State_Type is (ON, OFF);
 
+   --  A scheduler day and time.
+   type Day_Time is record
+      Day     : Hestia.Time.Day_Name;
+      Hour    : Hestia.Time.Hour_Number;
+      Minute  : Hestia.Time.Minute_Number;
+   end record;
+
+   --  Compare two scheduler day and time.
+   function "<" (Left, Right : in Day_Time) return Boolean;
+
+   --  Add some minutes to the scheduler day and time.
+   function "+" (Date    : in Day_Time;
+                 Minutes : in Hestia.Time.Minute_Number) return Day_Time;
+
+   --  Subtract two scheduler day and time and get the difference in minutes.
+   function "-" (Left, Right : in Day_Time) return Integer;
+
+   Unit : constant Hestia.Time.Minute_Number;
+
    --  The scheduler index.
    subtype Scheduler_Index is Hestia.Ports.Zone_Type;
 
    function Get_State (Date : in Hestia.Time.Date_Time;
                        Zone : in Scheduler_Index) return State_Type;
+
+   --  Get the scheduler state for the given day and time.
+   function Get_State (Date : in Day_Time;
+                       Zone : in Scheduler_Index) return State_Type;
+
+   --  Set the scheduler state for the given day and time.
+   procedure Set_State (Date  : in Day_Time;
+                        Zone  : in Scheduler_Index;
+                        State : in State_Type)
+     with Post => Get_State (Date, Zone) = State;
 
    --  Display the heat schedule on the display based on the current date and time.
    procedure Display (Buffer : in out HAL.Bitmap.Bitmap_Buffer'Class;
@@ -37,7 +66,9 @@ package Hestia.Scheduler is
 
 private
 
-   type Schedule_Unit is new Natural range 0 .. 86_400 / (5 * 60);
+   Unit : constant Hestia.Time.Minute_Number := 5;
+
+   type Schedule_Unit is new Natural range 0 .. 86_400 / ((Natural (Unit) * 60));
 
    type Day_Schedule is array (Schedule_Unit) of State_Type with Pack;
 
